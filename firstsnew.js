374535cb838f501c4bda766a9f403457ae04c9ac
rivka99 <rivkafl99@gmail.com>
2016-08-05 15:08:28 -0400

    Add files via upload

---
+++ firstsnew.js
@@ -0,0 +1,23 @@
+alert("welcome to my site, I coded it myself and I hope you find it to be quite delightful");
+window.alert("This site was made for my family and friends if you dont know why you are on this site its better that you leave");
+var picome = document.getElementById("12");
+function nextpic() {
+var firstpicofme= document.getElementById("pic");
+firstpicofme.src = "https://scontent.xx.fbcdn.net/v/t1.0-1/p160x160/12009555_131297727219618_7665782181034197931_n.jpg?oh=5a6dd06bc27b3810bb8350ed22b61cdc&oe=582F0CB3";
+firstpicofme.alt= " this is another picture of me";
+}
+function nextpic2() {
+var firstpicofme= document.getElementById("pic");
+firstpicofme.src = "https://scontent.xx.fbcdn.net/v/t1.0-0/p206x206/12190093_10206812760269312_1767059341676305152_n.jpg?oh=88155ebeda3b285be9560aa2f8630f47&oe=581775F0";
+firstpicofme.alt= " this is the first picture of me";
+}
+function picover() {
+var byebyepic= document.getElementById("bye");
+byebyepic.src = "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSI5OtkVvt7Sfu7Z5ANU9sgVTVJ5zKTP4oXEyi2xgRIjMJsRRpp";
+byebyepic.alt = " another goodbye image";
+}
+function changebc() {
+var byebyepic= document.getElementById("bye");
+byebyepic.src ="http://4.bp.blogspot.com/-Wb8WVOGUJK4/UGEsr9IJfeI/AAAAAAAABg8/QGLm4D285fI/s1600/Saying_bye_smiley_face.jpg";
+byebyepic.alt = "first goodbye picture";
+}
